48f4062eda80b7dd44dcd6db5751d73ec0297ccf
Sylvain Laperche <sylvain.laperche@scality.com>
2019-04-24 14:32:54 +0200

    tests: move Kube API check to an upper level
    These steps will be needed by the tests under `install` as well.
    
    Refs: #982
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- tests/post/steps/conftest.py
+++ tests/post/steps/conftest.py
@@ -1,30 +1,12 @@
 # -*- coding: utf-8 -*-
-from pytest_bdd import (
-    given,
-    then,
-    parsers,
-)
+from pytest_bdd import given, parsers
 
 from tests import kube_utils
-
-
-# Helpers
-def _verify_kubeapi_service(host):
-    """Verify that the kubeapi service answer"""
-    with host.sudo():
-        cmd = "kubectl --kubeconfig=/etc/kubernetes/admin.conf cluster-info"
-        retcode = host.run(cmd).rc
-        assert retcode == 0
 
 
 # Pytest-bdd steps
 
 # Given
-@given("the Kubernetes API is available")
-def check_service(host):
-    _verify_kubeapi_service(host)
-
-
 @given(parsers.parse("pods with label '{label}' are '{state}'"))
 def check_pod_state(host, label, state):
     pods = kube_utils.get_pods(
@@ -34,9 +16,3 @@ def check_pod_state(host, label, state):
     assert len(pods) > 0, "No {} pod with label '{}' found".format(
         state.lower(), label
     )
-
-
-# Then
-@then("the Kubernetes API is available")
-def verify_kubeapi_service(host):
-    _verify_kubeapi_service(host)
