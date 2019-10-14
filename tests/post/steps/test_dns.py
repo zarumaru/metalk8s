45695341df77c77a1a21217b2cf1c6d1caf1bdf9
Claude Ebaneck <claude.ebaneck@scality.com>
2019-10-14 14:37:17 +0200

    add delay interval

--- tests/post/steps/test_dns.py
+++ tests/post/steps/test_dns.py
@@ -30,7 +30,7 @@ def utils_pod(k8s_client, utils_image):
         kube_utils.wait_for_pod(
             k8s_client, name=pod_name, namespace="default", state="Running"
         ),
-        times=10,
+        times=12,
         wait=5,
         name="wait for Pod '{}'".format(pod_name),
     )
