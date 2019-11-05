6a0e29ea2d51d087e8b3471c0df645e5eef617c6
Alexandre Allard <alexandre.allard@scality.com>
2019-11-05 11:21:22 +0100

    test use worker uuid as VM id, also use stage name as prefix

--- eve/workers/openstack-multiple-nodes/terraform/common.tf
+++ eve/workers/openstack-multiple-nodes/terraform/common.tf
@@ -3,13 +3,19 @@ variable "worker_uuid" {
   default = ""
 }
 
+variable "stage_name" {
+  type    = string
+  default = ""
+}
+
 resource "random_string" "current" {
   length  = 5
   special = false
 }
 
 locals {
-  prefix = "metalk8s-${
+  prefix = "${
+    var.stage_name != "" ? var.stage_name : "metalk8s" }-${
     var.worker_uuid != "" ? var.worker_uuid : random_string.current.result
   }"
 }
