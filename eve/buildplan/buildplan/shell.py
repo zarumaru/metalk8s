1b0cbec1de7a47aa807f01f4bfa05cfa5246b9b6
Guillaume Demonet <guillaume.demonet@scality.com>
2019-12-05 21:25:10 +0100

    ci/buildplan: Add helpers to generate ShellCommand
    We define two wrappers of `core.ShellCommand` to gather common patterns
    into simple arguments.
    We also write a bunch of simple helpers to generate shell logic (such as
    `_and`, `_or`, or `_for`).

---
+++ eve/buildplan/buildplan/shell.py
@@ -0,0 +1,89 @@
+"""Helpers to manipulate `core.ShellCommand`."""
+
+from buildplan import core
+
+
+# Helpers {{{
+def _fmt_args(*args, join_with=" "):
+    """Join an iterable of `Optional[str]`, omitting `None`s."""
+    return join_with.join(str(arg) for arg in args if args is not None)
+
+
+def _and(*args):
+    return _fmt_args(*args, join_with=" && ")
+
+
+def _or(*args):
+    return _fmt_args(*args, join_with=" || ")
+
+
+def _seq(*args):
+    return _fmt_args(*args, join_with="; ")
+
+
+def _for(values_in, command, var="varname"):
+    values = (
+        values_in
+        if isinstance(values_in, str)
+        else " ".join(map(str, values_in))
+    )
+
+    return _seq('for {var} in {values}', "do {command}", "done").format(
+        var=var, values=values, command=command,
+    )
+
+
+def _if(predicate, _then, _else=None):
+    return _seq(
+        'if "{predicate}"',
+        "then {_then}",
+        "else {_else}" if _else is not None else None,
+        "done",
+    ).format(predicate=predicate, _then=_then, _else=_else)
+
+
+# }}}
+# Steps {{{
+class Shell(core.ShellCommand):
+    STEP_NAME = "ShellCommand"
+
+    def __init__(self, name, command, sudo=False, **kwargs):
+        if sudo:
+            command = "sudo " + command
+
+        self._kwargs = kwargs
+        super(Shell, self).__init__(name, command, **kwargs)
+
+
+class Bash(Shell):
+    def __init__(
+        self, name, command, *args, inline=False, wrap_env=False, **kwargs
+    ):
+        if inline:
+            if args:
+                raise ValueError(
+                    "Cannot pass positional arguments to `Bash` "
+                    "if using `inline=True`."
+                )
+            full_command = "bash -c '{}'".format(command)
+
+        else:
+            inner_command = _fmt_args(command, *args)
+
+            if wrap_env and "env" in kwargs:
+                command = "env {values} {command}".format(
+                    values=_fmt_args(
+                        *(
+                            "{}={}".format(key, value)
+                            for key, value in kwargs["env"].items()
+                        )
+                    ),
+                    command=inner_command,
+                )
+
+            full_command = "bash " + inner_command
+
+        super(Bash, self).__init__(name, full_command, **kwargs)
+
+
+# }}}
--- eve/buildplan/main.py
+++ eve/buildplan/main.py
@@ -3,6 +3,7 @@ import sys
 
 from buildplan import core
 from buildplan import dsl
+from buildplan import shell
 from buildplan import yamlprint
 
 
@@ -154,16 +155,17 @@ def set_version_property():
     return core.SetPropertyFromCommand(
         "Set version as property from built artifacts",
         property_name="metalk8s_version",
-        command=(
-            "bash -c '"
-            '. <(curl -s "%(prop:artifacts_private_url)s")'
-            " && echo $VERSION'"
+        command="bash -c '{}'".format(
+            shell._and(
+                '. <(curl -s "%(prop:artifacts_private_url)s")',
+                "echo $VERSION",
+            )
         ),
     )
 
 
 def build_all():
-    return core.ShellCommand(
+    return shell.Shell(
         "Build everything",
         command="./doit.sh -n 4",
         env={"PYTHON_SYS": "python3.6"},
@@ -173,7 +175,7 @@ def build_all():
 
 
 def build_docs():
-    return core.ShellCommand(
+    return shell.Shell(
         "Build documentation",
         command="tox --workdir /tmp/tox -e docs -- html latexpdf",
         env={"READTHEDOCS": "True"},
@@ -182,7 +184,7 @@ def build_docs():
 
 
 def lint_all():
-    return core.ShellCommand(
+    return shell.Shell(
         "Run all linting targets",
         command="./doit.sh lint",
         use_pty=True,
