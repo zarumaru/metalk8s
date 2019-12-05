f258a9a1bf44f4cdbe168cbae962522c039e7753
Guillaume Demonet <guillaume.demonet@scality.com>
2019-12-05 15:34:25 +0100

    fixup! ci: Introduce library to generate CI build plan

--- eve/buildplan/buildplan/tests/test_yamlprint.py
+++ eve/buildplan/buildplan/tests/test_yamlprint.py
@@ -2,10 +2,8 @@ from collections import OrderedDict
 
 from buildplan import yamlprint
 
+
 def test_dump_ordered_dict():
-    dataset = OrderedDict([
-        ('key1', 'value1'),
-        ('key2', 'value2'),
-    ])
+    dataset = OrderedDict([("key1", "value1"), ("key2", "value2"),])
 
     assert yamlprint.dump(dataset) == "key1: value1\nkey2: value2\n"
